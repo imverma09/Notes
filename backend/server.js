3f5781e136212112f359941e19496833483be114
imverma09 <imverma45@gmail.com>
2024-10-15 21:15:17 +0530

    set cors origin

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app =  express()
 const port = 4000 ;
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin:[ "http://localhost:3000", "https://notes-app11.vercel.app"],
       credentials: true, 
     })
   );
